f5ae06f79bf224b132c42001c1d33b7c318d146a
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-09-05 16:07:43 -0500

    deleted server example

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//$ npm install
-//$ npm run build
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDom = require("react-dom");
-ReactDom.render(React.createElement("h1", null, "Playlist Generator"), document.querySelector('#root'));
