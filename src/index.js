5ac2198e6b9e88fa0383393256c6c44a8049f380
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-09-05 15:56:24 -0500

    configured webpack. now using webpack to compile the project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDom = require("react-dom");
+ReactDom.render(React.createElement("h1", null, "This is JSX"), document.querySelector('#root'));
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(
+    <h1>This is JSX</h1>,
+    document.querySelector('#root')
+);
