a55a4b96f0d49ec14e399ca830a33e6333e2f153
Alexander Bachmann <alexander.d.bachmann@gmail.com>
2020-09-13 17:53:23 -0500

    tidied up the components but still having a problem where the api is called, just not actually mapped properly.. i think

--- spotify-playlist-generator/src/recommendations.js
+++ spotify-playlist-generator/src/recommendations.js
@@ -4,32 +4,25 @@ import { SpotifyApiContext, BrowseRecommendations } from 'react-spotify-api';
 import Cookies from 'js-cookie'
 
 function Recommendations(props) {
-  
-    const token = Cookies.get('spotifyAuthToken');
-  
+   const token = Cookies.get('spotifyAuthToken');
    return (
+        <SpotifyApiContext.Provider value={token}>
+            <BrowseRecommendations
+                options={{
+                    seed_artists:
+                        '4frXpPxQQZwbCu3eTGnZEw'
+                }}>
 
-    <SpotifyApiContext.Provider value={token}>
-
-
-        <p>You are authorized with token: {token}</p>
-
-        <BrowseRecommendations
-            options={{
-                seed_artists:
-                    '4frXpPxQQZwbCu3eTGnZEw'
-            }}>
-            {({ recommendations }) => (
-                recommendations ? (
-                    recommendations.tracks.map(track => (
-                        <h1 key={track.id}>{track.name}</h1>
-                    ))
-                ) : null
-            )
-            }
-        </BrowseRecommendations> 
-
-    </SpotifyApiContext.Provider>
+                {({ recommendations }) => (
+                    recommendations ? (
+                        recommendations.tracks.map(track => (
+                            <h1 key={track.id}>{track.name}</h1>
+                        ))
+                    ) : <h1>no recommendations</h1> 
+                )
+                }
+            </BrowseRecommendations> 
+        </SpotifyApiContext.Provider>
   );
 }
 
--- spotify-playlist-generator/src/signInButton.js
+++ spotify-playlist-generator/src/signInButton.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { SpotifyApiContext, BrowseRecommendations, Artist, Track} from 'react-spotify-api'
 import { SpotifyAuth } from 'react-spotify-auth'
 import Cookies from 'js-cookie'
 import 'react-spotify-auth/dist/index.css'
-
-import Recommendations from './recommendations';
 
 function SignInButton() {
 
@@ -13,27 +10,8 @@ function SignInButton() {
     return (
         <div className='app'>
         {token ? (
-            <SpotifyApiContext.Provider value={token}>
-            
-            <p>You are authorized with token: {token}</p>
-            
-            {/* Your Spotify Code here */}
-
-            <Artist id={["1XpDYCrUJnvCo9Ez6yeMWh","7jy3rLJdDQY21OgRLCZ9sD"]}>
-                
-                {({ artists }) => (
-                    artists ? (
-                        artists.map(artist => (
-                            <h1 key={artist.id}>{artist.name}</h1>
-                        ))
-                    ) : null
-                )}
-            </Artist>
-
-                {/* <Recommendations /> */}
-
-            </SpotifyApiContext.Provider>
-        ) : (
+           <h1>Signed in</h1> 
+            ) : (
             // Display the login page
             <SpotifyAuth
             redirectUri='http://localhost:3000/callback'
